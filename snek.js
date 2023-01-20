abfcfcd5abf8672f15fdba0d7637967cb096c2db
emmieykao <emmieykao@gmail.com>
2023-01-20 11:56:00 -0800

    finished game

--- snek.js
+++ snek.js
@@ -2,33 +2,54 @@ var c = document.getElementById("mainCanvas");
 var ctx = c.getContext("2d");
 var width = 20;
 var height = 20;
-var current_index = 0;
-var speed = 0.8;
-var interval_time = 0;
-var interval = 0;
 var sq_array;
-var snek_array;
+var snek_array = new Array(height);
 var should_run;
 var snake_run;
+var eat_sound;
+var apple_counter = 0;
 // [x direction, y direction, x coord, y coord]
 var current_snek = new Array(2);
-current_snek[0] = [1, 0, 10, 10];
-current_snek[1] = [1, 0, 11, 10];
+current_snek[0] = [1, 0, width / 2, height / 2];
+current_snek[1] = [1, 0, width / 2 + 1, height / 2];
+
+function sound(src) {
+    // CREDITS TO W3SCHOOLS FOR SOUND FUNCTION
+    this.sound = document.createElement("audio");
+    this.sound.src = src;
+    this.sound.setAttribute("preload", "auto");
+    this.sound.setAttribute("controls", "none");
+    this.sound.style.display = "none";
+    document.body.appendChild(this.sound);
+    this.play = function(){
+      this.sound.play();
+    }
+    this.stop = function(){
+      this.sound.pause();
+    }
+  }
 
 function create_snek_array(){
-    let snek_array = new Array(height);
     for(let i = 0; i < height; i++){
         snek_array[i] = new Array(width);
         for(let j = 0; j < width; j++){
-        snek_array[i][j] = 0;
+            snek_array[i][j] = 0;
         }
     }
+    snek_array[current_snek[0][3]][current_snek[0][2]] = 1;
     return snek_array;
 }
 
 function random_apple(){
     xRand = Math.floor(Math.random() * 20);
     yRand = Math.floor(Math.random() * 20);
+    var cnt = 0;
+    while(snek_array[yRand][xRand] == 1){
+        cnt += 1;
+        xRand = Math.floor(Math.random() * 20);
+        yRand = Math.floor(Math.random() * 20); 
+    }
+    console.log(cnt);
     ctx.fillStyle = "red";
     new_sq = ctx.fillRect(xRand * 30, yRand * 30, 30, 30);
     return [xRand, yRand];
@@ -36,27 +57,29 @@ function random_apple(){
 
 function init_snek(current_snek){
     sq_array = draw_board();
-    snek_array = create_snek_array();
+    create_snek_array();
     [app_x, app_y] = random_apple();
     sq_array[app_x][app_y] += 2;
     ctx.fillStyle = "blue";
     ctx.fillRect(current_snek[0][2] * 30, current_snek[0][3] * 30, 30, 30);
     ctx.fillRect(current_snek[1][2] * 30, current_snek[1][3] * 30, 30, 30);
-    snek_array[current_snek[0][2] - 1][current_snek[0][3] - 1] += 1;
-    snek_array[current_snek[1][2] - 1][current_snek[1][3] - 1] += 1;
     return [sq_array, snek_array];
 }
 
 function move_snek(){ 
     let col_num = current_snek.length - 1;
-    console.log(snek_array);
-
-    if(snek_array[current_snek[col_num][2]][current_snek[col_num][3]] == 1){
-        console.log("you hit something");
+    if(snek_array[current_snek[col_num][3]][current_snek[col_num][2]] == 1){
+        lose_sound.play();
+        you_lose();
         clearInterval(snake_run);
+        return;
     }
-
+    else {
+        snek_array[current_snek[col_num][3]][current_snek[col_num][2]] += 1;
+    }
     if(sq_array[current_snek[col_num][2]][current_snek[col_num][3]] >= 2) {
+        apple_counter += 1;
+        eat_sound.play();
         sq_array[current_snek[col_num][2]][current_snek[col_num][3]] -= 2;
         [x, y] = random_apple();
         sq_array[x][y] += 2;
@@ -65,22 +88,23 @@ function move_snek(){
     else if(sq_array[current_snek[0][2]][current_snek[0][3]] == 0){
         ctx.fillStyle = "#93B7F2";
         ctx.fillRect(current_snek[0][2] * 30, current_snek[0][3] * 30, 30, 30);
-        snek_array[current_snek[col_num][2]][current_snek[col_num][3]] -= 1;
+        snek_array[current_snek[0][3]][current_snek[0][2]] -= 1;
         current_snek.shift();
     }
     else if(sq_array[current_snek[0][2]][current_snek[0][3]] == 1){
         ctx.fillStyle = "#A5C4F7";
         ctx.fillRect(current_snek[0][2] * 30, current_snek[0][3] * 30, 30, 30);
-        snek_array[current_snek[col_num][2]][current_snek[col_num][3]] -= 1;
+        snek_array[current_snek[0][3]][current_snek[0][2]] -= 1;
         current_snek.shift();
     }
-    current_snek.push([current_snek[col_num - 1][0], current_snek[col_num - 1][1], current_snek[col_num -1][2] + current_snek[col_num - 1][0], current_snek[col_num - 1][3] + current_snek[col_num - 1][1]]);
-    snek_array[current_snek[col_num - 1][2]][current_snek[col_num - 1][3]] += 1;
+    col_num = current_snek.length - 1;
+    current_snek.push([current_snek[col_num][0], current_snek[col_num][1], current_snek[col_num][2] + current_snek[col_num ][0], current_snek[col_num][3] + current_snek[col_num][1]]);
     ctx.fillStyle = "blue";
-    ctx.fillRect(current_snek[col_num][2] * 30, current_snek[col_num][3] * 30, 30, 30);
-    should_run = check_for_hits(col_num);
+    ctx.fillRect(current_snek[col_num + 1][2] * 30, current_snek[col_num + 1][3] * 30, 30, 30);
+    should_run = check_for_hits(col_num + 1);
     if(should_run){
-        console.log("you hit something");
+        lose_sound.play();
+        you_lose();
         clearInterval(snake_run);
     }
 }
@@ -119,9 +143,9 @@ function onKeyPress(e){
 function check_for_hits(col_num) {
     if(   
         (current_snek[col_num][0] + current_snek[col_num][2] > width && current_snek[col_num][0] == 1) ||
-        (current_snek[col_num][0] + current_snek[col_num][2] < 0 && current_snek[col_num][0] == -1) ||
-        (current_snek[col_num][1]+ current_snek[col_num][3] > height && current_snek[col_num][1] == 1) ||
-        (current_snek[col_num][1] + current_snek[col_num][3] < 0 && current_snek[col_num][1] == -1)
+        (current_snek[col_num][0] + current_snek[col_num][2] < -1 && current_snek[col_num][0] == -1) ||
+        (current_snek[col_num][1] + current_snek[col_num][3] > height && current_snek[col_num][1] == 1) ||
+        (current_snek[col_num][1] + current_snek[col_num][3] < -1 && current_snek[col_num][1] == -1)
     )
     {
         return true;
@@ -131,9 +155,19 @@ function check_for_hits(col_num) {
     }
 }
 
+function you_lose(){
+    ctx.font = "30px Times New Roman";
+    ctx.fillStyle = "red";
+    ctx.textAlign = "center";
+    ctx.fillText("You hit something :(", width * 15, height * 10)
+    ctx.fillText("You ate " + apple_counter + " apples.", width * 15, height * 15);
+}
+
 function main(){
     myCanvas.addEventListener("keydown", onKeyPress);
-    [sq_array, snek_array] = init_snek(current_snek);
+    eat_sound = new sound("mixkit-chewing-something-crunchy-2244.wav");
+    lose_sound = new sound("mixkit-losing-drums-2023.wav")
+    init_snek(current_snek);
     snake_run = setInterval(move_snek, 200);
 }
 main();
